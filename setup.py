d5af2ed14aa3b70cfcbb06d1966b4b15fbf46334
Piotr Kaleta <piotr@counsyl.com>
2013-03-15 15:38:13 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='glacier-cli',
+      version='0.1',
+      packages=['glacier-cli'],
+      license='BSD',
+      description='Command-line interface to Amazon Glacier',
+      classifiers=[]
+)
